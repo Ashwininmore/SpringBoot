56c964ca8571699532345e164980cd7352917ca6
Ashwini Nitin More <69009147+Ashwininmore@users.noreply.github.com>
2024-01-13 12:23:18 +0530

    JDBC-simple-application

---
+++ JDBCApplication/src/main/java/com/example/demo/JdbcController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JdbcController {
+	
+	@Autowired
+	JdbcTemplate jtemp;
+	@RequestMapping("/insert")
+	public String appdata() {
+		jtemp.update("insert into user values('swami','Nutan')");
+		return "<h1>Record Inserted...</h1>";
+	}
+	@RequestMapping("/delete")
+	public String delete() {
+		jtemp.update("delete from user where Name='Ashu'");
+		return "<h1>Record Deleted...</h1>";
+	}
+	
+	
+}
